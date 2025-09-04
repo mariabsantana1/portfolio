66cba92944c47a61eec118813597e458be18d991
mariabsantana <mariabsantana7@gmail.com>
2025-09-04 09:02:08 -0300

    script animation scroll

--- script.js
+++ script.js
@@ -28,3 +28,78 @@ function headerShadow() {
 
   }
 }
+
+/* typing effect */
+var typingEffect = new Typed(".typedText",{
+  strings : ["Front-End Developer", "Web Developer", "Programmer"],
+  loop : true,
+  typeSpeed : 100,
+  backSpeed : 80,
+  backDelay : 2000
+})
+
+/* scroll animation */
+const sr = ScrollReveal({
+      origin: 'top',
+      distance: '80px',
+      duratin: 2000,
+      reset: true
+})
+
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social-icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+sr.reveal('.projects-card',{interval: 200})
+
+sr.reveal('.top-header',{})
+
+/* scroll reveal left-right animation */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duratin: 2000,
+  reset: true
+})
+
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duratin: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+  const scrollY = window.scrollY;
+
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 50,
+        sectionId = current.getAttribute('id');
+
+      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+      }else{
+
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+      }
+  })
+}
+
+window.addEventListener('scroll', scrollActive);
